40b4f17beeb5200f077432bc6b15ad989e338c2e
adrpeiser <serrapeiroa@gmail.com>
2024-09-17 12:39:39 +0200

    gerard

--- E2/src/Main.java
+++ E2/src/Main.java
@@ -1,23 +1,25 @@
 class Notification {
     void show(int num) {
+        System.out.println("You have " + num + " new messages");
 
 
-        if (num > 99){
-            System.out.println("You have +99 new messages");
-        }
-        else if (num == 1){
-            System.out.println("You have " + num + " new messages");
-        }
-        else {
-            System.out.println("You have " + num + " new messages");
-        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        Notification notification = new Notification();
+        Notification notification = new Notification(){
+            void show(int num) {
+                if (num > 99) {
+                    System.out.println("You have +99 new messages");
+                } else if (num == 1) {
+                    System.out.println("You have " + num + " new messages");
+                } else {
+                    System.out.println("You have " + num + " new messages");
+                }
+            }
+        };
 
         notification.show(1);
         notification.show(10);
